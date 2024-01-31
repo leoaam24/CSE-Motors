7dc7e170716356ab9e00c3fa03a518273b7fe776
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2024-01-31 19:08:28 +0800

    additional correction

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -44,7 +44,6 @@ validate.registrationRules = () => {
  * ***************************** */
 validate.checkRegData = async (req, res, next) => {
     const { account_firstname, account_lastname, account_email } = req.body
-    console.log(account_firstname)
     let errors = []
     errors = validationResult(req)
     if (!errors.isEmpty()) {
